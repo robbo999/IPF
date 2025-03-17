28049d6cfde3a1e0fc3ef26460a54fcf4040e6fc
robbo999 <118767105+robbo999@users.noreply.github.com>
2025-03-17 11:50:05 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ipf/", // Ensure GitHub Pages or Netlify detects the correct path
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: "public/index.html",
+    },
+  },
 });
